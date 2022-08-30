1ea95ecd6a4583e847776a4107761f562f8a5373
Dave Lucia <davelucianyc@gmail.com>
2022-08-30 13:23:29 -0400

    Initial commit

---
+++ lib/tab.ex
@@ -0,0 +1,18 @@
+defmodule Tab do
+  @moduledoc """
+  Documentation for `Tab`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Tab.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tab_test.exs
@@ -0,0 +1,8 @@
+defmodule TabTest do
+  use ExUnit.Case
+  doctest Tab
+
+  test "greets the world" do
+    assert Tab.hello() == :world
+  end
+end
